6621a5f74335ea2f2dff0e338e26a5e84598b991
unknown <muhammadfudailramadhani>
2022-11-03 08:23:04 +0700

    dashboard

--- src/Pages/dashboard.jsx
+++ src/Pages/dashboard.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='p-10 '>
+      <div>
+        <p className='font-serif p-4 '> Hai Royyan</p>
+      </div>
+      <div className='grid grid-cols-2'>
+      <div class="grid grid-cols-3 gap-4 flex-auto  ">
+        <div className='bg-slate-600 flex-'>3</div>
+        <div className=' bg-slate-600 '>2</div>
+        <div className=' bg-slate-600'>2</div>
+        <div className=' bg-slate-600'>2</div>
+        <div className=' bg-slate-600'>2</div>
+        <div className=' bg-slate-600'>2</div>
+        <div className=' bg-slate-600'>2</div>
+        <div className=' bg-slate-600'>2</div>
+      </div>
+      <div class="container bg-green-600 w-72   p-7 m-24">
+       
+       
+      </div>
+      </div>
+      
+      <div>
+
+      </div>
+    </div>
   )
 }
 
