89a0f09e45f2b05be33538a7c7b39f8d2cdf8f5f
muhyiii <muhyiii47@gmail.com>
2022-11-04 09:48:22 +0700

    dashboard persen sama dikit goals

--- src/Pages/SemiPage/addGoal.jsx
+++ src/Pages/SemiPage/addGoal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CgClose } from "react-icons/cg";
 
 const AddGoals = (props) => {
   if (!props.addGoals) {
@@ -11,13 +12,16 @@ const AddGoals = (props) => {
         variant="primary"
         className=" bg-black bg-opacity-50 z-20 modal absolute inset-0 flex justify-center items-center overflow-y-hidden h-full"
       >
-        <div className="w-1/5 h-2/3 bg-white">
-            <div>
-                asdasda
-            </div>
-            <button 
+        <div className="w-2/6 h-4/5 bg-white rounded-lg shadow-lg relative ">
+          <div
+            className="top-3 right-3 absolute cursor-pointer"
             onClick={props.onClose}
-            >ada</button>
+          >
+            <CgClose size={30} />
+          </div>
+          <div className="pt-6 text-2xl font-semibold text-center">
+            <h1>Add Goals</h1>
+          </div>
         </div>
       </div>
     </div>
